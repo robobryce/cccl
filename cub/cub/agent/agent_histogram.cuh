83b9f277337e3944c043b89ce5e567a959103a80
Bryce Adelstein Lelbach <brycelelbach@gmail.com>
2026-07-10 16:08:08 +0000

    Revert "[cub] Validate histogram cache with libcu++ atomics"
    This reverts commit c7c177fd97c2f41ca1d6cb44735eb7314251d426.

--- cub/cub/agent/agent_histogram.cuh
+++ cub/cub/agent/agent_histogram.cuh
@@ -23,7 +23,6 @@
 #include <cub/iterator/cache_modified_input_iterator.cuh>
 #include <cub/util_type.cuh>
 
-#include <cuda/atomic>
 #include <cuda/std/__host_stdlib/ostream>
 #include <cuda/std/__type_traits/conditional.h>
 #include <cuda/std/__type_traits/integral_constant.h>
@@ -369,8 +368,7 @@ struct AgentHistogram
 
         if (output_bin >= 0)
         {
-          ::cuda::atomic_ref<OutputCounterT, ::cuda::thread_scope_device>{d_output_histograms[ch][output_bin]}
-            .fetch_add(static_cast<OutputCounterT>(count), ::cuda::memory_order_relaxed);
+          atomicAdd(&d_output_histograms[ch][output_bin], static_cast<OutputCounterT>(count));
         }
       }
     }
